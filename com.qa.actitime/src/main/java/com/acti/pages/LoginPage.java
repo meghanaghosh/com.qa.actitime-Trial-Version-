c3109aac31899791ef99c17754d57b7a9845c9cc
meghanaghosh <meghanaghosh@192.168.1.35>
2020-07-30 17:52:57 -0400

    Second Commit - Meghana

--- com.qa.actitime/src/main/java/com/acti/pages/LoginPage.java
+++ com.qa.actitime/src/main/java/com/acti/pages/LoginPage.java
@@ -3,7 +3,6 @@ package com.acti.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.PageFactoryFinder;
 
 import com.acti.base.DriverScript;
 /*
